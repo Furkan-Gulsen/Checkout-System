f170ec179df22a24dec8cf2cb6e29e56c6b2af43
Furkan-Gulsen <m.furkangulsen@hotmail.com>
2024-01-06 11:33:06 +0300

    Remove unused entity files

--- internal/entities/vasItem.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type VasItem struct {
-	ID         int     `json:"id" bson:"_id"`
-	CategoryId int     `json:"categoryId" bson:"categoryId"`
-	SellerId   int     `json:"sellerId" bson:"sellerId"`
-	Price      float64 `json:"price" bson:"price"`
-	Quantity   int     `json:"quantity" bson:"quantity"`
-}
