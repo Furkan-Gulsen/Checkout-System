b753605a7c5ccfdd61b0e17c0bfe7d6bae87e095
Furkan-Gulsen <m.furkangulsen@hotmail.com>
2024-01-13 11:07:39 +0300

    Remove unused code related to the validator interface

--- internal/domain/repository/cart.go
+++ internal/domain/repository/cart.go
@@ -2,12 +2,6 @@ package repository
 
 import "github.com/Furkan-Gulsen/Checkout-System/internal/domain/entity"
 
-// type CartRepositoryI interface {
-// 	ApplyPromotion(cartId int, promotionId int) error
-// 	Display(cartId int) (entity.Cart, error)
-// 	ResetCart(cartId int) error
-// }
-
 type CartRepositoryI interface {
 	Create(cart *entity.Cart) (*entity.Cart, error)
 	GetByID(id int) (*entity.Cart, error)
